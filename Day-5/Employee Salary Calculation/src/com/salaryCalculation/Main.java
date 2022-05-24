2883cf6ad1675fe59352caf976a95ccbe3ec014e
manojkumar <manojkumarmhala@gmail.com>
2022-05-24 09:30:17 +0530

    java unit-4 day-4 assingment

--- Day-5/Employee Salary Calculation/src/com/salaryCalculation/Main.java
+++ Day-5/Employee Salary Calculation/src/com/salaryCalculation/Main.java
@@ -1,9 +1,19 @@
 package com.salaryCalculation;
-
+  import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
-		Employee e=Employee.getEmployeeDetails(101,"Vivek",20000.0);
-		int pfper=Employee.getPFPercentage(7);
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter Id");
+		int id=s.nextInt();
+		System.out.println("Enter name");
+		String name=s.next();
+		System.out.println("Enter salary");
+		int salary=s.nextInt();
+		System.out.println("Enter PF percentage:");
+		int pf=s.nextInt();
+		
+		Employee e=Employee.getEmployeeDetails(id,"name",salary);
+		int pfper=Employee.getPFPercentage(pf);
 		e.calculateNetSalary(pfper);
 		System.out.println(e.getEmployeeId());
 		System.out.println(e.getEmployeeName());
--- Day-5/Ticket Price Calculation/src/com/ticketPriceCalculaton/Main.java
+++ Day-5/Ticket Price Calculation/src/com/ticketPriceCalculaton/Main.java
@@ -1,41 +1,30 @@
 package com.ticketPriceCalculaton;
-
+ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the available tickets:");
+		int ticketAvilable=s.nextInt();
 		Ticket t1=new Ticket();
-		t1.setAvailableTickets(25);
-		t1.setPrice(100);
-		t1.setTicketid(123);
-		
-		
-		System.out.println("available tickets: "+t1.getAvailableTickets());
-		System.out.println("price per ticket: "+t1.getPrice());
-		
-		int numberOfTicketBooking=5;
-		
-		int totalPrice=t1.calculateTicketCost(numberOfTicketBooking);
-		System.out.println("Number of tickt booked :"+numberOfTicketBooking);
-		System.out.println("ticket id: "+t1.getTicketid());
-		System.out.println("total price: "+totalPrice);
-		System.out.println("available tickets: "+t1.getAvailableTickets());
-		
-		System.out.println("-----------------------------------------");
-		
-		Ticket t2=new Ticket();
-		t2.setPrice(100);
-		t2.setTicketid(124);
-		
-		System.out.println("available tickets: "+t2.getAvailableTickets());
-		System.out.println("price per ticket: "+t2.getPrice());
-		
-		numberOfTicketBooking=2;
-		
-		totalPrice=t2.calculateTicketCost(numberOfTicketBooking);
-		System.out.println("Number of ticket booked: "+numberOfTicketBooking);
-		System.out.println("ticket id: "+t2.getTicketid());
-		System.out.println("total price: "+totalPrice);
-		System.out.println("available tickets: "+t2.getAvailableTickets());
+		t1.setAvailableTickets(ticketAvilable);
+		while(ticketAvilable>0) {
+			System.out.println("Enter no of bookings: ");
+			int numberOfBookings=s.nextInt();
+			System.out.println("Enter the price");
+			int price=s.nextInt();
+			System.out.println("Enter the ticketid: ");
+			int ticketId=s.nextInt();
+			
+			int numberOfTicketBooking=numberOfBookings;
+			t1.setTicketid(ticketId);
+			t1.setPrice(price);
+			System.out.println("available tickets: "+t1.getAvailableTickets());
+			int totalPrice=t1.calculateTicketCost(numberOfTicketBooking);
+			
+			System.out.println("Total amount: "+totalPrice);
+			System.out.println("available tickets after booking: "+t1.getAvailableTickets());
+		}
+		s.close();
 	}
 }
